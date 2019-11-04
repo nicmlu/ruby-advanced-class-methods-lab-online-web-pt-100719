3d45254d1de0a0c875569774cf3876afa0c1e3f5
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-04 03:12:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
     song = self.new
     row = file_name.split("-")
     # song_data = row.split(".mp3")
-    song.artist_name = song_data[0]
-    song.name = song_data[1]
+    # song.artist_name = song_data[0]
+    # song.name = song_data[1]
   end
 
   def self.all
