a35b25c090b80de6b51ca8b8ac2f4926d1c090d3
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-04 02:19:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,10 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
+  
+  def self.alphabetical 
+    self.all.sort_by()
+  end
 
   def self.all
     @@all
